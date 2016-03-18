8bed14cba11253126f2da6a1dcbe8bdf7bcb5636
ZenykK <the.nick92@gmail.com>
2016-03-18 18:02:03 +0200

    - added sample classes for ping, echo, list commands

--- src/main/java/cons_interp/interp_main_class.java
+++ src/main/java/cons_interp/interp_main_class.java
@@ -4,12 +4,33 @@ import java.io.IOException;
 import java.io.Console;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class interp_main_class {
 
-	public static void main(String[] args) throws IOException {
-		// defining variables
+	static void ping (Socket socket) throws IOException, UnknownHostException{
 		
+		System.out.print("Ping successfull");
+	}
+	
+	static void echo (Socket socket, String[] args) throws IOException{
+		
+		System.out.print("Reply:" + echo_message);
+		
+		catch (IOException e) {
+	    	e.printStackTrace();
+	    	System.out.print("\nInvalid input!");
+	    }
+	}
+	
+	static void list (Socket socket) throws IOException, UnknownHostException{
+		
+		System.out.print("Ping successfull");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Socket socket = new Socket ("0.0.0.0", 151);		
 		System.out.print("Hello there!");
 		Console console = null;
 		String input = null;
